d2851880271119a95529ef9a179c00055bb5afba
TimG <timg4567@yahoo.com>
2015-05-07 05:53:18 -0400

    contact form testing

--- forms/contact.php
+++ forms/contact.php
@@ -6,10 +6,15 @@ $subject = $_POST["subject"];
 $message = $_POST["message"];
 
 $message = $fullName + "\r\n" + $email + "\r\n\r\n" + $message;
-
 $message = wordwrap($message, 70);
 
-mail("tribalmultisport@gmail.com", $subject, $message);
+if( isset($_POST['destinationOverride']) ) {
+	// Need to be able to test the form at least once.
+	mail("timg456789@yahoo.com", $subject, $message);
+}
+else {
+	mail("tribalmultisport@gmail.com", $subject, $message);
+}
 
 header('/pages/contact.html?formStep=thankYou');
 
